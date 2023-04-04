9000e10711e41d1ace340538e99b5a36092a0176
Pavel22062003 <sir.iakowlew-pawel@yandex.ru>
2023-04-04 19:36:51 +0200

    change paths

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,17 +1,21 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
 import psycopg2
 import csv
+import os
 from datetime import date
-path_to_employees = 'C:\\Users\\Пользователь\\OneDrive\\Рабочий стол\\postgres-homeworks\\homework-1\\north_data\\employees_data.csv'
-path_to_customers = 'C:\\Users\\Пользователь\\OneDrive\\Рабочий стол\\postgres-homeworks\\homework-1\\north_data\\customers_data.csv'
-path_to_orders = 'C:\\Users\\Пользователь\\OneDrive\\Рабочий стол\\postgres-homeworks\\homework-1\\north_data\\orders_data.csv'
+path_to_employees = os.path.join('north_data','employees_data.csv')
+path_to_customers = os.path.join('north_data','customers_data.csv')
+path_to_orders =  os.path.join('north_data','orders_data.csv')
 with psycopg2.connect(host='localhost',database='north',user='postgres',password='Persik012') as conn:
     with conn.cursor() as cur:
 
             with open(path_to_employees, 'r', encoding="UTF-8", newline='') as csvfile:
                 reader = csv.DictReader(csvfile, delimiter=',')
+
                 for row in reader:
+
                     thedate = date.fromisoformat(row['birth_date'])
+
                     cur.execute('INSERT INTO employees VALUES (%s,%s,%s,%s,%s)',(row['first_name'],row['last_name'],row['title'],thedate,row['notes']))
             with open(path_to_customers,'r',encoding='UTF=8',newline='') as customers:
                 reader_customers = csv.DictReader(customers,delimiter=',')
